a25844df20456c3e5f929ba24513c09337ed1d65
Tushar Verma <tushar.verma2319@gmail.com>
2023-04-04 22:27:48 +0530

    Updating Resume

--- src/components/2.Home/Data.jsx
+++ src/components/2.Home/Data.jsx
@@ -57,7 +57,7 @@ const Data = () => {
         </h1>
         <h3 className="home__subtitle">CSE UNDER GRAD</h3>
         <p className="home__description">
-            I am a student in Chitkara University, Baddi. I am pursing B.E. in computer science engineering. This is my first project. THANKU YOU 
+            I am a student in Chitkara University, Baddi. I am pursing B.E. in computer science engineering. This is my first project.
         </p>
 
         <a href="#contact" className="button button--flex">
